24490fb209f5133301e1491e8659b26783b04814
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-06-09 20:25:30 +0600

    add create admin api

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 //=================================
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_KEY}@cluster0.ih5yxul.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -54,7 +54,19 @@ async function run() {
         res.send(result);
     })
 
+    app.patch('/users/admin/:id', async (req, res)=>{
+      const id = req.params.id;
+      const filter = {_id : new ObjectId(id)}
+      const updateDoc ={
+        $set:{
+          role: 'admin'
+        },
+      };
 
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
 
 
 
