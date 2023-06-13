5101d774e2bd0ac0e2e035fb01f1b2ae867d1223
mehadi-shuvo <mehadihasanshuvo88@gamil.com>
2023-06-14 00:29:40 +0600

    comment for vercel

--- index.js
+++ index.js
@@ -53,7 +53,7 @@ const paymentsCollection = client.db('summerCamp').collection('payments');
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    //await client.connect();
 
     const verifyAdmin = async (req, res, next) => {
       const email = req.decoded.email;
